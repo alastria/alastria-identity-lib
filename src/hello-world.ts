f2429770282b744b3e3ee188a4de8b259f086f8a
postigo1993 <a.gomez.postigo@gmail.com>
2019-06-03 13:56:43 +0200

    alastria ip

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -2,7 +2,7 @@ import Web3 from 'web3';//Se comprueba que web3-lib esta en node-modules.
 
 var ABI_PresentationRegistry = [{"constant": true,"inputs": [	{"name": "subjectStatus","type": "uint8"	},	{"name": "receiverStatus","type": "uint8"	}],"name": "getPresentationStatus","outputs": [	{"name": "","type": "uint8"	}],"payable": false,"stateMutability": "pure","type": "function"	},	{"constant": false,"inputs": [	{"name": "receiverPresentationHash","type": "bytes32"	},	{"name": "status","type": "uint8"	}],"name": "updateReceiverPresentation","outputs": [],"payable": false,"stateMutability": "nonpayable","type": "function"	},	{"constant": false,"inputs": [	{"name": "subjectPresentationHash","type": "bytes32"	},	{"name": "URI","type": "string"	}],"name": "addSubjectPresentation","outputs": [],"payable": false,"stateMutability": "nonpayable","type": "function"	},	{"constant": true,"inputs": [	{"name": "subject","type": "address"	},	{"name": "subjectPresentationHash","type": "bytes32"	}],"name": "getSubjectPresentationStatus","outputs": [	{"name": "exists","type": "bool"	},	{"name": "status","type": "uint8"	}],"payable": false,"stateMutability": "view","type": "function"	},	{"constant": true,"inputs": [],"name": "version","outputs": [	{"name": "","type": "int256"	}],"payable": false,"stateMutability": "view","type": "function"	},	{"constant": true,"inputs": [],"name": "previousPublishedVersion","outputs": [	{"name": "","type": "address"	}],"payable": false,"stateMutability": "view","type": "function"	},	{"constant": true,"inputs": [	{"name": "receiver","type": "address"	},	{"name": "receiverPresentationHash","type": "bytes32"	}],"name": "getReceiverPresentationStatus","outputs": [	{"name": "exists","type": "bool"	},	{"name": "status","type": "uint8"	}],"payable": false,"stateMutability": "view","type": "function"	},	{"constant": false,"inputs": [	{"name": "subjectPresentationHash","type": "bytes32"	},	{"name": "status","type": "uint8"	}],"name": "updateSubjectPresentation","outputs": [],"payable": false,"stateMutability": "nonpayable","type": "function"	},	{"constant": true,"inputs": [],"name": "getSubjectPresentationList","outputs": [	{"name": "","type": "uint256"	},	{"name": "","type": "bytes32[]"	}],"payable": false,"stateMutability": "view","type": "function"	},	{"inputs": [	{"name": "_previousPublishedVersion","type": "address"	}],"payable": false,"stateMutability": "nonpayable","type": "constructor"	},	{"anonymous": false,"inputs": [	{"indexed": false,"name": "hash","type": "bytes32"	},	{"indexed": false,"name": "status","type": "uint8"	}],"name": "PresentationUpdated","type": "event"	}];
 
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const web3 = new Web3(new Web3.providers.HttpProvider("http://0.0.0.0/rpc"));
 web3.eth.defaultAccount = web3.eth.accounts[0];
 
 var presentationRegistry = new web3.eth.Contract(ABI_PresentationRegistry, "0x8e78E1BfBdcD1564309d86d4925fCF533a6dcBC8");
@@ -23,10 +23,26 @@ export function addSubjectPresentationRegistry(subjectPresentationHash, URI, fro
 /*
 export function updateSubjectPresentationRegistry(subjectPresentationHash, status, from){
   presentationRegistry.methods.updateSubjectPresentation(subjectPresentationHash, status).send({from: from});
-  var event = presentationRegistry.PresentationUpdated({}, from:, to:)
+  var result = presentationRegistry.setFinished.call(true,{from: web3.eth.accounts[0], gas:3000000}, function(err, res){ console.log(res) });
+  presentationRegistry.updateSubjectPresentation.sendTransaction(address_to, subjectPresentationHash, {from: address_from, gas:200000},
+    function (error,result){
+      if (!error){
+        var event = presentationRegistry.PresentationUpdated({},{fromBlock:'latest', toBlock:'latest'),
+        function(error, result){
+          if (!error){
+            var msg = "OK! La empresa " + result.args._from + " ha emitido " + result.args._n + " tokens al empleado " + result.args._to;
+            imprimir(msg);
+          }else{
+            console.log("Error" + error);
+          }
+        }
+        });
+      } else {
+        console.error("Error" + error);
+      }
   console.log('Se ha actualizado la presentation' + subjectPresentationHash);
-
 }
+
 export function getSubjectPresentationRegistryStatus(subject, subjectPresentationHash, from){
   presentationRegistry.methods.getSubjectPresentationStatus(subject, subjectPresentationHash).call({from: from});
   console.log('Se ha obtenido correctamente el estado de la presentation' + subjectPresentationHash + 'del sujeto' + subject);

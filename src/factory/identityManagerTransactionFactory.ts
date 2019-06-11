c19860e7a8dd3430678f5e54e3866f0d812edbed
AliciaLombarte <alicia.lombarte.garcia@gmail.com>
2019-06-11 16:14:33 +0200

    Add AlastriaPresentationRegistry contract functions to correspondent alastria-identity-lib

--- src/factory/identityManagerTransactionFactory.ts
+++ src/factory/identityManagerTransactionFactory.ts
@@ -1,51 +1,111 @@
 //With web3 v1.0.0 the encode can be done with web3.eth.abi.encodeFunctionCall(jsonInterface,parameters)
 //TODO: change encoding when v1.0.0 releases stable version
-import { toHex, leftPad } from "../../node_modules/web3-utils";
+import { toHex, leftPad, rightPad } from "../../node_modules/web3-utils";
+
+// TODO: import contract address from configfile
+const publicKeyRegistry = '0x0b337E2aC98a9725615dE042E950dD8C8b66b0fA';
+const credentialRegistry = '0xE4f91b47399Dc2560025Aafb4fFA7Cd5C483330e';
+const presentationRegistry = '0x8e78E1BfBdcD1564309d86d4925fCF533a6dcBC8';
+
+const generateAccessTokenFunctionHash = '4284f8d4';
+const createAlastriaIdentityFunctionHash = '6d69d99a';
+const addSubjectCredentialFunctionHash = 'e04ce02c';
+const addSubjectPresentationFunctionHash = '4e3a5de5';
+const updateSubjectPresentationFunctionHash = 'e64af938';
+const updateReceiverPresentationFunctionHash = '3000dc39';
+
+
+
 
 const alastriaIdentityManager = '0xf18bd0f5a4f3944f3074453ce2015e8af12ed196';
 const basicTransaction = {
-    from: '',
-    to: alastriaIdentityManager,
-    data: '',
-    gas: 0,
-    nonce: ''
+  from: '',
+  to: alastriaIdentityManager,
+  data: '',
+  gas: 0,
+  nonce: ''
 }
 const zeroValue = '00000000000000000000000000000000000000000000000000000000000000000000';
 const delegateCallSignature = '597b2e9b';
 const delegateCallInvoke = (registryAddress) => { return `${delegateCallSignature}000000000000000000000000${registryAddress.slice(2, 40)}${zeroValue}` }
 
-export function generateAccessToken(){
-    return {};
-  }
-  
-  export function createAlastriaIdentity(){
-    return {};
-  }
-  
-  export function createIdentity(publicKey){
-    let transaction = basicTransaction;
-    transaction.data = `0x6d69d99a
+
+
+export function generateAccessToken(signAddress) {
+  let transaction = basicTransaction;
+  transaction.data = `0x${delegateCallInvoke(alastriaIdentityManager)}
+  ${generateAccessTokenFunctionHash}
+  ${leftPad(signAddress.slice(2), 64)}`;
+  transaction.gas = 600000;
+  return transaction;
+}
+
+export function createAlastriaIdentity(publicKeyData) {
+  let transaction = basicTransaction;
+  transaction.data = `0x${delegateCallInvoke(alastriaIdentityManager)}
+  ${createAlastriaIdentityFunctionHash}
+  0000000000000000000000000000000000000000000000000000000000000020
+  ${leftPad(toHex(publicKeyData.length).slice(2), 64)}
+  ${rightPad(toHex(publicKeyData).slice(2), 64)}`;
+  transaction.gas = 600000;
+  return transaction;
+}
+
+export function createIdentity(publicKey) {
+  let transaction = basicTransaction;
+  transaction.data = `0x6d69d99a
     0000000000000000000000000000000000000000000000000000000000000020
-    ${leftPad(publicKey.length,64)}
+    ${leftPad(publicKey.length, 64)}
     60e6cfd87fdf96e9f8749d267319088775ad1cc245e5cd9fa0d6567426788a3e
     a10e675e00000000000000000000000000000000000000000000000000000000`;
-    transaction.gas = 200000;
-    return transaction;
-  }
-  
+  transaction.gas = 200000;
+  return transaction;
+}
+
 /**
- * 
- * @param registryAddress
  * @param presentationHash should have 32 bytes
  */
-const addSubjectPresentationSignature = '4e3a5de5';
 const addSubjectPresentationCost = 200000;
-export function addSubjectCredential(registryAddress, presentationHash, uri) {
-    let transaction = basicTransaction;
-    transaction.data =
-        `0x${delegateCallInvoke(registryAddress)}${addSubjectPresentationSignature}${presentationHash}
+export function addSubjectCredential(presentationHash, uri) {
+  let transaction = basicTransaction;
+  transaction.data =
+    `0x${delegateCallInvoke(credentialRegistry)}${addSubjectCredentialFunctionHash}${presentationHash}
     0000000000000000000000000000000000000000000000000000000000000040
     ${leftPad(uri.length, 64)}${toHex(uri)}`;
-    transaction.gas = addSubjectPresentationCost;
-    return transaction;
+  transaction.gas = addSubjectPresentationCost;
+  return transaction;
+}
+
+// PresentationRegistry.sol
+
+export function addSubjectPresentation(subjectPresentationHash, uri) {
+  let transaction = basicTransaction;
+  transaction.data = `0x${delegateCallInvoke(presentationRegistry)}
+  ${addSubjectPresentationFunctionHash}
+  ${rightPad(subjectPresentationHash.slice(2), 64)}
+  0000000000000000000000000000000000000000000000000000000000000040
+  ${leftPad(toHex(uri.length).slice(2), 64)}
+  ${rightPad(toHex(uri).slice(2), 64)}`;
+  transaction.gas = 600000;
+  return transaction;
+}
+
+export function updateSubjectPresentation(subjectPresentationHash, status) {
+  let transaction = basicTransaction;
+  transaction.data = `${delegateCallInvoke(presentationRegistry)}
+  ${updateSubjectPresentationFunctionHash}
+  ${rightPad(subjectPresentationHash.slice(2), 64)}
+  ${leftPad(toHex(status).slice(2), 64)}`;
+  transaction.gas = 600000;
+  return transaction;
+}
+
+export function updateReceiverPresentation(receiverPresentationHash, status) {
+  let transaction = basicTransaction;
+  transaction.data = `${delegateCallInvoke(presentationRegistry)}
+  ${updateReceiverPresentationFunctionHash}
+  ${rightPad(receiverPresentationHash.slice(2), 64)}
+  ${leftPad(toHex(status).slice(2), 64)}`;
+  transaction.gas = 600000;
+  return transaction;
 }
--- src/factory/presenationRegistrytransactionFactory.ts
+++ src/factory/presenationRegistrytransactionFactory.ts
@@ -1,41 +1,49 @@
 //With web3 v1.0.0 the encode can be done with web3.eth.abi.encodeFunctionCall(jsonInterface,parameters)
 //TODO: change encoding when v1.0.0 releases stable version
-import { toHex, leftPad } from "../../node_modules/web3-utils";
+import { toHex, leftPad, rightPad } from "../../node_modules/web3-utils";
 
 const basicTransaction = {
-    from: '',
-    to: '',
-    data: '',
-    gas: 0,
-    nonce: ''
+  from: '',
+  to: '',
+  data: '',
+  gas: 0,
+  nonce: ''
+}
+const getSubjectPresentationStatusFunctionHash = '0x5205080f';
+const getSubjectPresentationListFunctionHash = '0xee47717f';
+const getReceiverPresentationStatusFunctionHash = '0x9373014a';
+const getPresentationStatusFunctionHash = '0x034f8408';
+
+export function getSubjectPresentationStatus(subject, subjectPresentationHash) {
+  let transaction = basicTransaction;
+  transaction.data = `${getSubjectPresentationStatusFunctionHash}
+  ${leftPad(subject.slice(2), 64)}
+  ${rightPad(subjectPresentationHash.slice(2), 64)}`;
+  transaction.gas = 600000;
+  return transaction;
 }
 
-const zeroValue = '00000000000000000000000000000000000000000000000000000000000000000000';
+export function getSubjectPresentationList() {
+  let transaction = basicTransaction;
+  transaction.data = `${getSubjectPresentationListFunctionHash}`;
+  transaction.gas = 600000;
+  return transaction;
+}
 
-export function getTransactionAddSubjectPresentation(){
-    return {};
-  }
-  
-  export function getTransactionUpdateSubjectPresentation(){
-    return {};
-  }
-  
-  export function getTransactionGetSubjectPresentationStatus(){
-    return {};
-  }
-  
-  export function getTransactionGetSubjectPresentationList(){
-    return {};
-  }
-  
-  export function getTransactionUpdateReceiverPresentation(){
-    return {};
-  }
-  
-  export function getTransactionGetReceiverPresentationStatus(){
-    return {};
-  }
-  
-  export function getTransactionGetPresentationStatus(){
-    return {};
-  }
+export function getReceiverPresentationStatus(receiver, receiverPresentationHash) {
+  let transaction = basicTransaction;
+  transaction.data = `${getReceiverPresentationStatusFunctionHash}
+  ${leftPad(receiver.slice(2), 64)}
+  ${rightPad(receiverPresentationHash.slice(32), 64)}`;
+  transaction.gas = 600000;
+  return transaction;
+}
+
+export function getPresentationStatus(subjectStatus, receiverStatus) {
+  let transaction = basicTransaction;
+  transaction.data = `${getPresentationStatusFunctionHash}
+  ${leftPad(toHex(subjectStatus).slice(2), 64)}
+  ${leftPad(toHex(receiverStatus).slice(2), 64)}`;
+  transaction.gas = 600000;
+  return transaction;
+}

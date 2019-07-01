f5ed8bf1201e3337cf0257fa5b47e226dc184ed4
Eduardo Sánchez Mata <eduardo.sanchez.mata@everis.com>
2019-07-01 12:29:53 +0200

    dist files are generated by typescript

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var transactionFactory_1 = require("./factory/transactionFactory");
-exports.transactionFactory = transactionFactory_1.transactionFactory;
-var transactionProcess_1 = require("./transactionProcess");
-exports.UserIdentity = transactionProcess_1.UserIdentity;

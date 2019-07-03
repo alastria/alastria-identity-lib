6737262036fbb63e14b9fb64b80ca16c572f924d
Eduardo Sánchez Mata <eduardo.sanchez.mata@everis.com>
2019-07-03 12:00:49 +0200

    Even with the shim, we need an index source

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+var transactionFactory_1 = require("./factory/transactionFactory");
+exports.transactionFactory = transactionFactory_1.transactionFactory;
+var transactionProcess_1 = require("./transactionProcess");
+exports.UserIdentity = transactionProcess_1.UserIdentity;

ba78a4350531ed74a77acf350d1582d6eab83d5f
msalitu <mariasalgadoitu@gmail.com>
2019-07-08 12:03:53 +0200

    add console.log to test

--- src/factory/tokensFactory.ts
+++ src/factory/tokensFactory.ts
@@ -10,19 +10,21 @@ export const tokensFactory = {
 // Used by Service Provider
 export function signPresentationRequest(presentationRequest) {
   //return presentationRequestJWT;
+  console.log('hi signPresentationRequest')
 }
 
 // Used by Subject Wallet
 export function verifyPresentationRequest(presentationRequestJWT) {
-
+  console.log('hi verifyPresentationRequest')
 }
 
 // Used by Subject Wallet
 export function signPresentation(presentation) {
   //return presentationJWT;
+  console.log('hi signPresentation')
 }
 
 // Used by Service Provider
 export function verifyPresentation(presentationJWT) {
-
+  console.log('hi verifyPresentation')
 }

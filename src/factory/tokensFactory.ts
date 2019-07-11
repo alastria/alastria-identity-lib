160b4c6a8efb7c8116d822fe18cf9146082aae03
Alberto Gomez Postigo <alberto.gomez@iecisa.com>
2019-07-11 09:24:56 +0200

    functions added to tokensFactory

--- src/factory/tokensFactory.ts
+++ src/factory/tokensFactory.ts
@@ -1,5 +1,5 @@
 //import { TokenVerifier } from 'jsontokens'
- const _: any = require('jsontokens');
+const _: any = require('jsontokens');
 export const tokensFactory = {
   presentation: {
     'signPresentationRequest': signPresentationRequest,
@@ -10,26 +10,35 @@ export const tokensFactory = {
 }
 
 // Used by Service Provider
-export function signPresentationRequest(presentationRequest) {
+export function signPresentationRequest(presentationRequest, rawPrivateKey) {
   //return presentationRequestJWT;
   console.log('hi signPresentationRequest')
-}
+  var jsonObject = new _.TokenSigner('ES256K', rawPrivateKey).sign(presentationRequest);
+  console.log('The token is: ' + jsonObject)
+  return jsonObject
+};
 
 // Used by Subject Wallet
 export function verifyPresentationRequest(presentationRequestJWT, rawPublicKey) {
   console.log('hi verifyPresentationRequest')
-  console.log('hi')
-  const jsonObject = new _.TokenVerifier('ES256K', rawPublicKey).verify(presentationRequestJWT)
+  var jsonObject = new _.TokenVerifier('ES256K', rawPublicKey).verify(presentationRequestJWT);
+  console.log(jsonObject)
   return jsonObject
 }
 
 // Used by Subject Wallet
-export function signPresentation(presentation) {
+export function signPresentation(presentation, rawPrivateKey) {
   //return presentationJWT;
   console.log('hi signPresentation')
+  var jsonObject = new _.TokenSigner('ES256K', rawPrivateKey).sign(presentation);
+  console.log('The token is: ' + jsonObject)
+  return jsonObject
 }
 
 // Used by Service Provider
-export function verifyPresentation(presentationJWT) {
+export function verifyPresentation(presentationJWT, rawPublicKey) {
   console.log('hi verifyPresentation')
+  var jsonObject = new _.TokenVerifier('ES256K', rawPublicKey).verify(presentationJWT);
+  console.log(jsonObject)
+  return jsonObject
 }

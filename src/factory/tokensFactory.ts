e813d404bbeb2157881a15ad41ee8dc5061e3dff
Alberto Gomez Postigo <alberto.gomez@iecisa.com>
2019-08-12 11:40:00 +0200

    new lib functions

--- src/factory/tokensFactory.ts
+++ src/factory/tokensFactory.ts
@@ -6,7 +6,14 @@ export const tokensFactory = {
     'verifyPresentationRequest': verifyPresentationRequest,
     'signPresentation': signPresentation,
     'verifyPresentation': verifyPresentation,
+    //'createCredential': createCredential,
     'decodeJWT': decodeJWT
+    //'signJWT': signJWT,
+    //'verifyJWT': verifyJWT,
+    //'createAlastriaSession': createAlastriaSession,
+    //'createAlastriaToken': createAlastriaToken,
+    //'createPresentation': createPresentation,
+    //'createPresentationRequest': createPresentationRequest
   }
 }
 
@@ -45,11 +52,51 @@ export function verifyPresentation(presentationJWT, rawPublicKey) {
   return tokenData
 }
 
+export function createCredential(credentialContext, levelAssurance, fieldName, fieldValue) {
+  const jwt = {
+    "@context": credentialContext,
+    "levelOfAssurance": levelAssurance,
+    fieldName: fieldValue
+  }
+  return jwt;
+}
+
 // Used by Service Provider or Subject Wallet
-export function decodeJWT(jsonObject) {
+export function decodeJWT(jwt) {
   var tokenData = null
-  if(jsonObject){
-    tokenData = _.decodeToken(jsonObject)
+  if(jwt){
+    tokenData = _.decodeToken(jwt)
   }
   return tokenData
 }
+/*
+function signJWT(jwt, rawPrivateKey) {
+  var jsonObject = new _.TokenSigner('ES256K', rawPrivateKey).sign(jwt);
+  return jsonObject
+}
+
+function verifyJWT(jwt, rawPublicKey) {
+  var jsonObject = new _.TokenVerifier('ES256K', rawPublicKey).verify(jwt);
+  var tokenData = null
+  if(jsonObject){
+    tokenData = _.decodeToken(jwt)
+  }
+  return tokenData
+}
+
+function createAlastriaSession(issuerDID, publicKeyUserWallet, verifiedJwt, tokenValidTime, setUpTokenTime) {
+
+  return jsonObject
+}
+
+function createAlastriaToken(issuerDID, gwu, cbu, tokenValidTime, setUpTokenTime) {
+  return jsonObject
+}*/
+
+function createPresentation(issuerDID, subjectDID, credentials, tokenValidTime, setUpTokenTime, tokenId) {
+  return jsonObject
+}
+
+/*function createPresentationRequest(issuerDID, subjectDID, objects, tokenValidTime, setUpTokenTime, tokenId) {
+  return jsonObject
+}*/

777330ee74385913d38577d75525a929b08e4548
Daniel de la Sota <danieldlsota@gmail.com>
2019-10-14 18:12:49 +0200

    return to delegated

--- src/txFactory/credentialRegistryTransactionFactory.ts
+++ src/txFactory/credentialRegistryTransactionFactory.ts
@@ -10,7 +10,8 @@ import { config } from '../config';
 
 export function addSubjectCredential(web3, subjectCredentialHash, URI) {
   let transaction = Object.assign({}, config.basicTransaction)
-  transaction.data = web3.eth.abi.encodeFunctionCall(config.contractsAbi["AlastriaCredentialRegistry"]["addSubjectCredential"], [subjectCredentialHash, URI]);
+  let delegatedData = web3.eth.abi.encodeFunctionCall(config.contractsAbi["AlastriaCredentialRegistry"]["addSubjectCredential"], [subjectCredentialHash, URI]);
+  transaction.data = delegated(web3, delegatedData)
   transaction.to = config.alastriaIdentityManager;  // When delegated, target is alastriaIdentityManager
   transaction.gasLimit = 600000;
   return transaction;
@@ -24,7 +25,8 @@ export function addSubjectCredential(web3, subjectCredentialHash, URI) {
  */
 export function deleteSubjectCredential(web3, subjectCredentialHash) {
     let transaction = Object.assign({}, config.basicTransaction)
-    transaction.data = web3.eth.abi.encodeFunctionCall(config.contractsAbi["AlastriaCredentialRegistry"]["deleteSubjectCredential"], [subjectCredentialHash]);
+    let delegatedData = web3.eth.abi.encodeFunctionCall(config.contractsAbi["AlastriaCredentialRegistry"]["deleteSubjectCredential"], [subjectCredentialHash]);
+    transaction.data = delegated(web3, delegatedData)
     transaction.to = config.alastriaIdentityManager;  // When delegated, target is alastriaIdentityManager
     transaction.gasLimit = 600000;
     return transaction;

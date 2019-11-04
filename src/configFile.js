526b2a645537d37e4f87f9aa32d5cd55c6e52be4
Daniel de la Sota <danieldlsota@gmail.com>
2019-11-04 11:35:03 +0100

    configFile cleaned

--- src/configFile.js
+++ src/configFile.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-// import * as fs from 'fs';
-// import * as path from 'path';
 const fs = require('fs');
 const path = require('path');
 
-
-//TODO getting from GitHub URL repository
-const contractsPath = '../alastria-identity/contracts'; // supposing alastria-identity is cloned at the same level as alastria-identity-lib
-//const contractsPath = '../alastria-identity/contracts/abi'
+const contractsPath = '../alastria-identity/contracts'; 
 var _contractsAbi = {};
 
 //Process the abi dir for getting an object with all the abi functions

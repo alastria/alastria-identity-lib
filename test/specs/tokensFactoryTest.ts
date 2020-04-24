c46e3bf50f31d973051831bf2a768be59e80119a
Nicolas Manero <nicolas.manero@4tic.com>
2020-04-24 10:39:09 +0200

    #71 Actualizado string del proxy por uno fake

--- test/specs/tokensFactoryTest.ts
+++ test/specs/tokensFactoryTest.ts
@@ -5,17 +5,17 @@ import { tokensFactory } from '../../src/tokenFactory/tokensFactory'
 describe('validate creatingDID results with custom networkID', function () {
     it('should return a valid DID with redT as a networkId', function () {
         const network = 'net';
-        const proxyAddress = 'proxy';
+        const proxyAddress = 'QmeeasCZ9jLbX...ueBJ7d7csxhb';
         const networkID = 'redT'
         const validDID = tokensFactory.tokens.createDID(network, proxyAddress, networkID);
-        expect(validDID).equal('did:ala:net:redT:proxy');
+        expect(validDID).equal('did:ala:net:redT:QmeeasCZ9jLbX...ueBJ7d7csxhb');
     });
 
     it('should return a valid DID with redB as a networkId', function () {
         const network = 'net';
-        const proxyAddress = 'proxy';
+        const proxyAddress = 'QmeeasCZ9jLbX...ueBJ7d7csxhb';
         const networkID = 'redB'
         const validDID = tokensFactory.tokens.createDID(network, proxyAddress, networkID);
-        expect(validDID).equal('did:ala:net:redB:proxy');
+        expect(validDID).equal('did:ala:net:redB:QmeeasCZ9jLbX...ueBJ7d7csxhb');
     });
 });

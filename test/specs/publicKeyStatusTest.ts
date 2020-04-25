c51b65eb9c17dbe5f8198ff62c2cdd1373ee9643
Nicolas Manero <nicolas.manero@4tic.com>
2020-04-25 13:54:35 +0200

    #47 Movemos el stub dentro del test

--- test/specs/publicKeyStatusTest.ts
+++ test/specs/publicKeyStatusTest.ts
@@ -6,22 +6,21 @@ const web3 = require('web3')
 const sinon = require('sinon')
 
 describe('validate public key in several situations with dates', function () {
-    before(() => {
+    it('should return false if the public key is not found', () => {
         sinon.stub(demo.transactionFactory.publicKeyRegistry, 'getPublicKeyStatusDecodedAsJSON')
             .callsFake(function fakeFn() {
                 return {
-                "exists": false,
-                "status": 0,
-                "startDate": 0,
-                "endDate": 0
+                    "exists": false,
+                    "status": 0,
+                    "startDate": 0,
+                    "endDate": 0
                 }
             });
-    });
-
-    it('should return false if the public key is not found', () => {
         const fakePublicKey = "0x00000000000000000000000000000000";
         const date = 1587815151740;
         const fakeSubject = "0x00000000000000000000000000000000";
         expect(demo.transactionFactory.publicKeyRegistry.isPublicKeyValidForDate(web3, fakeSubject, fakePublicKey, date)).to.be.false;
-    })
+
+        sinon.restore();
+    });
 });

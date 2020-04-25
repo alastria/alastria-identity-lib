2f4e38838fa68026ad2b2e46c48e629b9e369c42
Nicolas Manero <nicolas.manero@4tic.com>
2020-04-25 14:04:44 +0200

    #47 Añadido test que valida que si la fecha esta entre las de validez, devuelve true

--- test/specs/publicKeyStatusTest.ts
+++ test/specs/publicKeyStatusTest.ts
@@ -17,9 +17,27 @@ describe('validate public key in several situations with dates', function () {
                 }
             });
         const fakePublicKey = "0x00000000000000000000000000000000";
-        const date = 1587815151740;
+        const date = new Date('January 17, 1996 03:24:00').getTime();
         const fakeSubject = "0x00000000000000000000000000000000";
         expect(demo.transactionFactory.publicKeyRegistry.isPublicKeyValidForDate(web3, fakeSubject, fakePublicKey, date)).to.be.false;
+
+        sinon.restore();
+    });
+
+    it('should return true if the date sent by the user is between status dates', () => {
+        sinon.stub(demo.transactionFactory.publicKeyRegistry, 'getPublicKeyStatusDecodedAsJSON')
+            .callsFake(function fakeFn() {
+                return {
+                    "exists": true,
+                    "status": 0,
+                    "startDate": new Date('December 17, 1995 03:24:00').getTime(),
+                    "endDate": new Date('December 17, 1996 03:24:00').getTime()
+                }
+            });
+        const fakePublicKey = "0x00000000000000000000000000000000";
+        const date = new Date('January 17, 1996 03:24:00').getTime();
+        const fakeSubject = "0x00000000000000000000000000000000";
+        expect(demo.transactionFactory.publicKeyRegistry.isPublicKeyValidForDate(web3, fakeSubject, fakePublicKey, date)).to.be.true;
 
         sinon.restore();
     });

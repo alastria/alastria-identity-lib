ba9e305bafd639528aa827ee175332d5bce9005b
Max García <maximo.garcia@iecisa.com>
2020-10-05 12:13:37 +0200

    added legacy comment

--- src/tokenFactory/tokensFactory.ts
+++ src/tokenFactory/tokensFactory.ts
@@ -1,14 +1,27 @@
-import { createAlastriaSession } from './jwt/alastriaSession'
-import { createAlastriaToken } from './jwt/alastriaToken'
-import { createCredential } from './jwt/credential'
-import { createPresentation } from './jwt/presentation'
-import { createPresentationRequest } from './jwt/presentationRequest'
-import { createAIC } from './jwt/aic'
+// This files stays here only for legacy purposes
+// Functions used in this file have been moved to ~/src/tokenFactory/jwt
+
+import {
+  PSMHash,
+  createAIC,
+  createAlastriaSession,
+  createAlastriaToken,
+  createCredential,
+  createDID,
+  createPresentation,
+  createPresentationRequest,
+  decodeJWT,
+  signJWT,
+  verifyJWT
+} from './jwt'
 
 export const tokensFactory = {
   tokens: {
+    /** @deprecated Import it now as follows: ~/src/tokenFactory/jwt */
     decodeJWT,
+    /** @deprecated Import it now as follows: ~/src/tokenFactory/jwt */
     signJWT,
+    /** @deprecated Import it now as follows: ~/src/tokenFactory/jwt */
     verifyJWT,
     /** @deprecated Import it now as follows: ~/src/tokenFactory/jwt */
     createAlastriaSession,
@@ -20,9 +33,11 @@ export const tokensFactory = {
     createPresentation,
     /** @deprecated Import it now as follows: ~/src/tokenFactory/jwt */
     createPresentationRequest,
+    /** @deprecated Import it now as follows: ~/src/tokenFactory/jwt */
     PSMHash,
     /** @deprecated Import it now as follows: ~/src/tokenFactory/jwt */
     createAIC,
+    /** @deprecated Import it now as follows: ~/src/tokenFactory/jwt */
     createDID
   }
 }

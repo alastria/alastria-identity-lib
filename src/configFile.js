4b062993b61d8e7f3ddfffb8e19f5136fd23808b
Ángel <33127189+anthares101@users.noreply.github.com>
2021-04-26 13:10:29 +0200

    Use temp-alastriaID-truffle-contracts repository for getting the contracts

--- src/configFile.js
+++ src/configFile.js
@@ -2,7 +2,7 @@
 const fs = require('fs')
 const path = require('path')
 
-const contractsABIPath = '../upgradeable-contracts/abi'
+const contractsABIPath = '../temp-alastriaID-truffle-contracts/build/contracts'
 var _contractsAbi = {}
 
 // Process the abi dir for getting an object with all the abi functions
@@ -23,28 +23,10 @@ fs.readdirSync(path.join(__dirname, `${contractsABIPath}`)).forEach((file) => {
   _contractsAbi[abiFile.contractName] = abi
 })
 
-// // Read file ContractInfo.md and take rows to aobtain the addres of each contract
-// const contractsInfo = fs.readFileSync(
-//   path.join(__dirname, `${contractsPath}/`, 'ContractInfo.md'),
-//   'utf8'
-// )
-// const contractInfoRow = contractsInfo.split('\n')
-
-/*
-  FIXME load contract addresses from the ContractInfo.md file
-  once the file address are up to date
- */
-
-const contractsInfo = {
-  Eidas: '0x73238B556a3ebdD444e58cA67c70EE02423e7006',
-  AlastriaServiceProvider: '0x3edd539469d34BFacf77Ae0F271764594Cd1AF82',
-  AlastriaIdentityIssuer: '0xC3084402b7B87a218B19Ed7a8B9E963C370258c7',
-  AlastriaIdentityEntity: '0x1e7a94A63b04042B6302923f6AFA9Ccf1Ae0256C',
-  AlastriaCredentialRegistry: '0xe9B858A4b79fbAa0c6a7C69Ff56122639E4c1892',
-  AlastriaPresentationRegistry: '0x9FAdadE1734eFa76515BD49CBB4D26fa3d6C933c',
-  AlastriaPublicKeyRegistry: '0xfe611588Bb7699A38594B4f2E0AA5Ecb0E7a3641',
-  AlastriaIdentityManager: '0x6c60F15f4a30482Ba4769A21DF7727b9b69b2f90'
-}
+const contractsAddressesPath = '../temp-alastriaID-truffle-contracts'
+const contractsInfo = JSON.parse(
+  fs.readFileSync(path.join(__dirname, `${contractsAddressesPath}`, 'addresses.json'), 'utf8')
+)
 
 const config = {
   alastriaIdentityManager: contractsInfo.AlastriaIdentityManager,
